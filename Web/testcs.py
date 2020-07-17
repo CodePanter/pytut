e669c2f24e11187742848c52cbcd4dda349eae8f
delicateoutlier <delicateoutlier@gmail.com>
2020-07-17 16:43:02 +0200

    guess the number

---
+++ Web/testcs.py
@@ -0,0 +1,14 @@
+#a = ["banana","apple","chicken"]
+
+#for element in a :
+  #  print (element)
+
+b = [20,19,5]
+total = 0
+for e in b:
+    total = total + e
+    print(total)
+
+
+c= list (range(1,5))
+print (c)
