b32caa5746fe84e835926dd58b57b347fa30f9fc
delicateoutlier <delicateoutlier@gmail.com>
2020-07-20 12:59:25 +0200

    added if it is an odd or even number

--- Own code/GuessTheNumber2.py
+++ Own code/GuessTheNumber2.py
@@ -1,31 +1,38 @@
 import random
+guessestaken = 0
 
 print ('Welcome , what is your name ?')
 my_name = input()
 
+number = random.randint(1,20)
+print('Well,' + my_name + ',I am thinking about a number between 1 and 20.')
 
-print('Do you think it is an odd number or even number?')
-num = input()
+for guessesTaken in range(6):
+    print('Take a guess.')
+    guess = input()
+    guess = int (guess)
+    if (guess % 2) == 0:
+        print('{0} is even number '.format(guess))
+    else:
+        print('{0} is odd number'.format(guess))
 
-num= int (input('Enter your number'))
+    if guess < number:
+        print ('Your guess is too low.')
+
+    if guess > number :
+        print('Your guess is too high')
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken+1)
+    print('Good job,' + my_name + '!You guessed my number in ' +
+          guessesTaken + '  guessess!')
+
+if guess !=number:
+    number = str(number)
+    print('Nope. The number I was thinking of was  .' + number + '   ')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-##show that the guess is even or odd.
-if (num % 2) == 0:
-    print('{0} is even '.format(num))
-else:
-    print('{0} is odd'.format(num))
 
