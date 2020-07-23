268c6d8e87bfbeebc027a4fbf3f4c6cd0cf14749
delicateoutlier <delicateoutlier@gmail.com>
2020-07-23 15:36:17 +0200

    Currently following a tutorial for for loops

---
+++ Web/forloops.py
@@ -0,0 +1,19 @@
+a = ['banana','apple','microsoft']
+for element in a:       #for each element do the following is printing each element
+    print(element)
+    print(element)
+
+b = [20,10,5]
+total = 0           #add element total to list.
+for e in b:
+    print(e)
+    total = total + e
+    print(total)
+
+c= list(range(1,69))         #create a range from 1 until 69 without 6
+print (c)
+
+total2= 0           #new variable
+for i in range(1,83):
+    print(i)
+    total2 += i # the new value of variable 2 should be the sum of the old value of total 2 + i4
