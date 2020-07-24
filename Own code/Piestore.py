6683fdf5b7b0b5d04d5b6297d7c5f2a6b36678b6
delicateoutlier <delicateoutlier@gmail.com>
2020-07-24 16:46:25 +0200

    add a storyline at the piestore file

--- Own code/Piestore.py
+++ Own code/Piestore.py
@@ -1,26 +1,43 @@
+#Storyline : Pie store is busy so there need to be waiting time, after
+#waiting for a while the player can choose which pie and afterwards
+#the player play for a contest which he gets a random number and
+#if it's the lucky number he would get a surprise.
+
+
+
 import random
 import time
 
-#Intro for the store
 guessesTaken = 0
 
 print('''Welcome at the Raspberry Pie store, today we have a special offer. Pick a number and you get the special pie.''')
-
-
-def waiting():
-        print('Currently we are busy, please have patience')
-        time.waiting(5)
-        print()
+myNumber=input()
+print()
 
 def choosePie():
-        print('Sorry for the waiting time, which type of pie do you want?')
-        pie = ['Blueberry Pie', 'Chocolate cake', 'Raspberry pie','Apple pie']
-        print(pie)
-
-    while pie != 'Blueberry Pie' and 'Chocolate cake' and 'Raspberry pie' and 'apple pue':
-       print('Which cake do you want?')
+    pie = ''
+    while pie != 'Chocolate' and pie != 'Raspberry':
+        print('Which pie do you want? (Chocolate or Raspberry)')
         pie = input()
 
-       return pie
+        return pie
+
+
+#def waiting():
+       # print('Currently we are busy, please have patience')
+       # time.waiting(5)
+      #  print()
+
+
+#print('Sorry for the waiting time, which type of pie do you want?')
+#pie= input()
+
+#Pie = ['Blueberry Pie', 'Chocolate cake', 'Raspberry pie','Apple pie']
+#print()
+#while pie != ((Blueberry Pie) && (Chocolate cake) && (Raspberry pie) && (apple pie):
+     #  print('Which cake do you want?')
+      #  pie = input()
+
+      # return pie
 
 
