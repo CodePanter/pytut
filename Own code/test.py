6ba0aceb72e7cb853472b5c45869557f65b88008
delicateoutlier <delicateoutlier@gmail.com>
2020-07-24 16:56:51 +0200

    Tried to apply a nested while loop although I am still thinking about the structure of the pie store

--- Own code/test.py
+++ Own code/test.py
@@ -1,6 +1,6 @@
 import random
 import time
-
+#Random number , number connected to a pie.
 
 guessesTaken = 0
 
@@ -9,4 +9,20 @@ Pick a number and you get the special pie. May I know your name?''')
 myName = input()
 
 print('Excuse me , currently we are busy do you mind to wait for a second?')
-time.wait(3)
+time.sleep(3)
+
+if pie< 10:
+    if chocolatepie == 'Chocolate':
+        print('Chocolatepie')
+    else:
+        print('Chocolatebar')
+elif pie >= 11 and pie <20:
+    if pie == 'Raspberry':
+        print('RaspberryPie')
+    else:
+        print('CoconutPie')
+else:
+    if pie >=20 and pie <30:
+        print('Applepie')
+    else:
+        print('Blueberrypie')
