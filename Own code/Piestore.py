b0d887a019a7f70c6be7b0559afe3f7bc3313f96
delicateoutlier <delicateoutlier@gmail.com>
2020-07-26 19:39:02 +0200

    Connect the numbers to a pie so the user can pick a number

--- Own code/Piestore.py
+++ Own code/Piestore.py
@@ -10,17 +10,55 @@ import time
 
 guessesTaken = 0
 
-print('''Welcome at the Raspberry Pie store, today we have a special offer. Pick a number and you get the special pie.''')
-myNumber=input()
-print()
+print('''Welcome at the Raspberry Pie store, today we have a special offer. 
+Pick a number and you get the special pie.''')
 
-def choosePie():
-    pie = ''
-    while pie != 'Chocolate' and pie != 'Raspberry':
-        print('Which pie do you want? (Chocolate or Raspberry)')
-        pie = input()
+number = random.randint(1,5)
+for Guesstaken in range (5):
+    print('Pick a number between 1 and 5 and you will be surprised')
+    guess=input()
+    guess=int(guess)
+    if guess == 1:
+        print('Congratulations you will get a Chocolate pie.')
+    if guess == 2:
+        print('Congratulations you will get a Raspberry pie.')
+    if guess == 3:
+        print('Congratulations you will get a Blueberry pie.')
+    if guess == 4:
+        print('Congratulations you will get a Cinnamon cake.')
+    if guess == 5:
+        print('Congratulations you will get a Cheesecake.')
 
-        return pie
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############################
+#def choosePie():
+ #   pie = ''
+  #  while pie != 'Chocolate' and pie != 'Raspberry':
+   #     print('Which pie do you want? (Chocolate or Raspberry)')
+#  pie = input()
+#
+ #       return pie
 
 
 #def waiting():
