b5b436df0eab6aba56ace2fd6040d554517c204d
delicateoutlier <delicateoutlier@gmail.com>
2020-07-30 12:23:04 +0200

    learned about dictionaries

--- Web/dictionaries.py
+++ Web/dictionaries.py
@@ -4,3 +4,20 @@ d['Tom'] = 32
 d['Jenny'] = 21
 
 print(d['George']) #prints value of variable George
+print(d['Jenny'])
+
+d['Jenny'] = 20     #way to assign a new value to the variable
+print(d['Jenny'])
+
+
+#keys are commonly string or numbers
+d[10]=100
+print(d[10])
+
+#how to iterate over key-value pairs?
+for key, value in d.items():
+    print('Key')
+    print(key)
+    print ('Value:')
+    print(value)
+    print('')
