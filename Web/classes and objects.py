4e014723c09f8bee1900cebfedb58411a5e68e27
delicateoutlier <delicateoutlier@gmail.com>
2020-07-30 13:46:33 +0200

    learning about classes and objects and how to combine 2 classes

--- Web/classes and objects.py
+++ Web/classes and objects.py
@@ -20,6 +20,24 @@ class Robot:
 r1= Robot ('Tom','Red', 30)
 r2= Robot ('Jenny','Blue', 40)
 
+class Person:
+    def __init__(self,n,p,i,):
+        self.name = n
+        self.personality = p
+        self.is_sitting = i
 
-r1.introduce_self()
-r2.introduce_self()
+
+    def sit_down(self):
+        self.is_sitting = True
+    def stand_up(self):
+        self.is_sitting = False
+
+p1 = Person ('Alice','aggresive',False)     #false for is sitting
+p2 = Person ('Becky', 'talkative', True)
+
+
+#p1 owns r2
+p1.robot_owned = r2
+p2.robot_owned = r1
+
+p1.robot_owned.introduce_self()
