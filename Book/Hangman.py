5f7995a3485041e8ee9899d1142a6e739e053989
delicateoutlier <delicateoutlier@gmail.com>
2020-07-31 20:03:12 +0200

    Working on hangman

--- Book/Hangman.py
+++ Book/Hangman.py
@@ -34,3 +34,15 @@ HANGMAN_PICS =  [ '''
    /|\  |
    / \  |
        ===''']
+
+words = 'ant baboon badger beaver camel cat clam cobra cougar' \
+        'coyote crow deer dog donkey duck eagle ferret fox frog' \
+        'goat goose hawk lion lizzard llama mole monkey moose mouse' \
+        'mule newt otter owl panda parrot pigeon python rabbit ram rat' \
+        'raven rhino salmon seal shark sheep skunk sloth snake spider stork' \
+        'swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+
+def getRandomWord(wordlist):
+    #This function returns a random string from the passed list of strings.
+    wordIndex = random.randint(0, len(wordlist) -1 )
+    return wordlist[wordIndex]
