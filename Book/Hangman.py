af8dc309a97414073a1365d8dc3788f0690e9fe4
delicateoutlier <delicateoutlier@gmail.com>
2020-08-04 21:24:56 +0200

    add commentary at hangman

--- Book/Hangman.py
+++ Book/Hangman.py
@@ -42,7 +42,7 @@ words = 'ant baboon badger beaver camel cat clam cobra cougar' \
         'raven rhino salmon seal shark sheep skunk sloth snake spider stork' \
         'swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
 
-def getRandomWord(wordlist):
+def getRandomWord(wordlist):                                                    #defined a getRandomWord function, a list argument will be passed for its wordList parameter.This function will return a single secret word from the list in wordList.
     #This function returns a random string from the passed list of strings.
-    wordIndex = random.randint(0, len(wordlist) -1 )
-    return wordlist[wordIndex]
+    wordIndex = random.randint(0, len(wordlist) -1 )                             #Store a random index for this list in the wordIndex variable calling randint() with 2 arguments. First 0(for the first possible index) and the second is the value that the expression len(wordList)-1 evaluates to (for the last possible index in a wordlist.
+    return wordlist[wordIndex]                                                   #
